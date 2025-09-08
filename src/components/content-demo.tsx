16057c6df09a33f4b378285befc9f3104055c833
meerbahadin <meerbahadin10@gmail.com>
2025-09-08 10:43:36 +0300

    updated content

---
+++ src/components/content-demo.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from './ui/button'
+import { IconBrandGithub, IconDownload } from '@tabler/icons-react'
+
+const ContentDemo = () => {
+  return (
+    <>
+      {' '}
+      <div className='gap-4 flex flex-col items-center max-w-4xl'>
+        <div className='space-y-2 text-white text-center'>
+          <h1 className='bg-gradient-to-tr from-white via-white to-transparent bg-clip-text text-transparent text-4xl md:text-7xl font-bold leading-tight text-balance'>
+            Create Amazing Gradients Using WebGL
+          </h1>
+          <p className='md:text-xl leading-relaxed text-balance'>
+            High-performance React component for animated WebGL gradient
+            backgrounds with real-time customization.
+          </p>
+        </div>
+
+        <Link href='/#installation'>
+          <Button className='capitalize cursor-pointer'>
+            <IconDownload />
+            installation
+          </Button>
+        </Link>
+      </div>
+      <div className='flex w-full items-center justify-between gap-2 container'>
+        <Link href='https://www.meera.dev/' target='_blank'>
+          <Image
+            src='https://www.meera.dev/logo.svg'
+            width={28}
+            height={28}
+            alt='Meera Dev Logo'
+            className='w-8 h-8 mask-t-from-40%'
+          />
+        </Link>
+
+        <div className='flex gap-2'>
+          <Link target='_blank' href='https://github.com/meerbahadin/grad-flow'>
+            <Button
+              size='icon'
+              className='cursor-pointer dark text-foreground bg-background/30 hover:bg-background/50'
+            >
+              <IconBrandGithub />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ContentDemo
--- src/components/grad-flow-demo.tsx
+++ src/components/grad-flow-demo.tsx
@@ -1,13 +1,10 @@
 'use client'
 
 import { useEffect, useRef, useCallback, useMemo, useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
 import { Mesh, Program, Renderer, Transform, Plane } from 'ogl'
+
 import {
-  IconBrandGithub,
   IconCode,
-  IconDownload,
   IconScreenshot,
   IconSettings,
   IconWand,
@@ -30,6 +27,7 @@ import { cn } from '@/lib/utils'
 import { GradFlowProps, GradientConfig, GradientType, RGB } from './grad-flow'
 
 import { fragmentShader, vertexShader } from './grad-flow'
+import ContentDemo from './content-demo'
 
 const DEFAULT_CONFIG: GradientConfig = {
   color1: { r: 107, g: 85, b: 216 },
@@ -486,47 +484,7 @@ export default function GradFlowDemo({
         </div>
       </div>
 
-      <div className='gap-4 flex flex-col items-center max-w-4xl'>
-        <div className='space-y-2 text-white text-center'>
-          <h1 className='bg-gradient-to-tr from-white via-white to-transparent bg-clip-text text-transparent text-4xl md:text-7xl font-bold leading-tight text-balance'>
-            Create Amazing Gradients Using WebGL
-          </h1>
-          <p className='md:text-xl leading-relaxed text-balance'>
-            High-performance React component for animated WebGL gradient
-            backgrounds with real-time customization.
-          </p>
-        </div>
-
-        <Link href='/#installation'>
-          <Button className='capitalize cursor-pointer'>
-            <IconDownload />
-            installation
-          </Button>
-        </Link>
-      </div>
-
-      <div className='flex w-full items-center justify-between gap-2 container'>
-        <Link href='https://www.meera.dev/' target='_blank'>
-          <Image
-            src='https://www.meera.dev/logo.svg'
-            width={28}
-            height={28}
-            alt='Meera Dev Logo'
-            className='w-8 h-8 mask-t-from-40%'
-          />
-        </Link>
-
-        <div className='flex gap-2'>
-          <Link target='_blank' href='https://github.com/meerbahadin/grad-flow'>
-            <Button
-              size='icon'
-              className='cursor-pointer dark text-foreground bg-background/30 hover:bg-background/50'
-            >
-              <IconBrandGithub />
-            </Button>
-          </Link>
-        </div>
-      </div>
+      <ContentDemo />
 
       <canvas
         ref={canvasRef}
